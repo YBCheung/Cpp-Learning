9cddf756cb4551a9ccd8ebd616e01846c9217d4c
Yibo Zhang <576411472@qq.com>
2018-12-08 19:48:56 +0800

    Update 类.cpp

--- 类.cpp
+++ 类.cpp
@@ -1,22 +1,47 @@
 #include <iostream>
+using std::cin;
 using std::cout;
 using std::endl;
 
-class test
-{
-	int x,y;
-public:
-	test(int a=0,int b=1);  //此函数专门用来初始化private变量
+class Complex{
+	private: 
+		int real, imag;  //参数缺省则默认为private）
+	public:
+		Complex(void);
+		Complex(int a,int b = 0);
+		Complex(Complex c1,Complex c2);   //可有多个构造函数，根据形参类型匹配对应函数。
+		Display();
 };
-test::test(int a, int b)
+Complex::Complex(void) //构造函数直接对类进行初始化
 {
-	x=a;
-	y=b;
-	cout<<a<<endl<<b<<endl;
+	cout<<"real=";
+    cin>>real;
+    cout<<"imag=";
+    cin>>imag;
+}
+Complex::Complex(int a,int b) //构造函数直接对类进行初始化
+{
+	real = a;imag = b;
+}
+Complex::Complex(Complex c1,Complex c2) //构造函数直接对类进行初始化
+{
+	real = c1.real+c2.real;
+	imag = c1.imag+c2.imag;
+}
+Complex::Display(void)
+{	
+	cout<<real<<'+'<<imag<<'j'<<endl;
+	return 0;
 }
 
 int main(int argc, char const *argv[])
 {	
-	test x(1,2);
+	Complex c1;
+	Complex *c2 = new Complex(3,4);
+	Complex sum = Complex(c1,*c2);
+	c1.Display();
+	c2->Display();
+	sum.Display();
+	delete []c2;
 	return 0;
 }
